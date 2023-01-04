aca17d8b9bd9896e912630d9b0469148fcf858bc
thachdao31 <thachdao31@gmail.com>
2023-01-04 16:16:24 +0700

    update

--- Model/UserCheckinModel.js
+++ Model/UserCheckinModel.js
@@ -1,14 +1,6 @@
 const {getDb} = require('../connectDb');
 const {BaseModel} = require('./base');
 
-// class UserCheckinModel extends BaseModel {
-//     constructor() {
-//         super('histories');
-//     }
-// }
-
-// module.exports = new UserCheckinModel();
-
 function getHistoriesCollection() {
     return getDb().collection('histories')
 }
--- Model/UserModel.js
+++ Model/UserModel.js
@@ -1,5 +1,3 @@
-const {ObjectId} = require('mongodb');
-const {getDb} = require('../connectDb');
 const BaseModel = require('./base');
 
 class UserModel extends BaseModel {
@@ -9,27 +7,3 @@ class UserModel extends BaseModel {
 }
 
 module.exports = new UserModel();
-
-
-
-// function getUsersCollection() {
-//     return getDb().collection('users');
-// }
-
-// module.exports = {
-//     getUsers: () => {
-//         return getUsersCollection().find({}).toArray();
-//     },
-//     getUserById: (id) => {
-//         return getUsersCollection().findOne({_id: ObjectId(id)});
-//     },
-//     createUser: (user) => {
-//         return getUsersCollection().insertOne(user);
-//     },
-//     updateUser: (id, user) => {
-//         return getUsersCollection().updateOne({_id: ObjectId(id)}, {$set: user});
-//     },
-//     deleteUser: (id) => {
-//         return getUsersCollection().deleteOne({_id: ObjectId(id)});
-//     }
-// }
