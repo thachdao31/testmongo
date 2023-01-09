b9143428800a5dfd0f291cbd04b67aeae9aa5316
thachdao31 <thachdao31@gmail.com>
2023-01-09 16:54:20 +0700

    update

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const {connectToDb} = require('./connectDb');
 const UserRoute = require('./Route/UserRoute');
 const UserCheckinRoute = require('./Route/UserCheckinRoute');
+const cors = require('cors')
+
 
 app.use(express.json());
+app.use(cors())
 connectToDb();
 
 app.use('/api/users', UserRoute);
